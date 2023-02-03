4c6eb31f0186fd75b84c58d0de54109d59e5a6e3
shreya161 <106361774+shreya161@users.noreply.github.com>
2023-02-03 11:46:39 -0500

    added picture

--- src/views/IndexSections/Typography.js
+++ src/views/IndexSections/Typography.js
@@ -70,7 +70,7 @@ export default function Typography() {
               <img
                 alt="..."
                 className="img-fluid rounded shadow-lg"
-                src={require("assets/img/james.jpg")}
+                src={require("assets/img/minion.jpg")}
                 style={{ width: "150px" }}
               />
                <h1>
